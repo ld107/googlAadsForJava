b7523478f190993f3b60acb5ba2c56613cf673af
ld107 <835246255@qq.com>
2022-12-21 14:21:40 +0800

    授权文件 获取账户信息

--- google-ads-examples/src/main/java/com/google/ads/googleads/examples/AboutProjectForPlay/GetCustomerInfo.java
+++ google-ads-examples/src/main/java/com/google/ads/googleads/examples/AboutProjectForPlay/GetCustomerInfo.java
@@ -1,13 +1,13 @@
 package com.google.ads.googleads.examples.AboutProjectForPlay;
 
 import com.beust.jcommander.Parameter;
-import com.google.ads.googleads.examples.accountmanagement.GetAccountInformation;
-import com.google.ads.googleads.examples.basicoperations.GetCampaigns;
+import com.google.ads.googleads.examples.AboutProjectForPlay.mysql.MySqlConnt;
 import com.google.ads.googleads.examples.utils.ArgumentNames;
 import com.google.ads.googleads.examples.utils.CodeSampleParams;
 import com.google.ads.googleads.lib.GoogleAdsClient;
 import com.google.ads.googleads.v12.errors.GoogleAdsError;
 import com.google.ads.googleads.v12.errors.GoogleAdsException;
+import com.google.ads.googleads.v12.resources.AccountBudget;
 import com.google.ads.googleads.v12.resources.Customer;
 import com.google.ads.googleads.v12.services.GoogleAdsRow;
 import com.google.ads.googleads.v12.services.GoogleAdsServiceClient;
@@ -15,26 +15,20 @@ import com.google.ads.googleads.v12.services.SearchGoogleAdsStreamRequest;
 import com.google.ads.googleads.v12.services.SearchGoogleAdsStreamResponse;
 import com.google.api.gax.rpc.ServerStream;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GetCustomerInfo {
 
-    private static class GetAccountInformationParams extends CodeSampleParams {
-
-        @Parameter(names = ArgumentNames.CUSTOMER_ID, required = true)
-        private Long customerId;
-    }
-
     public static void main(String[] args) {
-        GetCustomerInfo.GetAccountInformationParams params = new GetCustomerInfo.GetAccountInformationParams();
-        if (!params.parseArguments(args)) {
-
-            // Either pass the required parameters for this example on the command line, or insert them
-            // into the code here. See the parameter class definition above for descriptions.
-            params.customerId = Long.parseLong("5877229176");
-        }
-
 
         GoogleAdsClient googleAdsClient = null;
         try {
@@ -49,7 +43,7 @@ public class GetCustomerInfo {
         }
 
         try {
-            new GetCustomerInfo().runExample(googleAdsClient, params.customerId);
+            new GetCustomerInfo().runExample(googleAdsClient);
         } catch (GoogleAdsException gae) {
             // GoogleAdsException is the base class for most exceptions thrown by an API request.
             // Instances of this exception have a message and a GoogleAdsFailure that contains a
@@ -70,10 +64,9 @@ public class GetCustomerInfo {
      * Runs the example.
      *
      * @param googleAdsClient the Google Ads API client.
-     * @param customerId      the client customer ID.
      * @throws GoogleAdsException if an API request failed with one or more service errors.
      */
-    private void runExample(GoogleAdsClient googleAdsClient, long customerId) {
+    private void runExample(GoogleAdsClient googleAdsClient) {
         try (GoogleAdsServiceClient googleAdsServiceClient =
                      googleAdsClient.getLatestVersion().createGoogleAdsServiceClient()) {
             // Constructs a query to retrieve the customer.
@@ -84,17 +77,14 @@ public class GetCustomerInfo {
                             + "customer.time_zone, "
                             + "customer.tracking_url_template, "
                             + "customer.auto_tagging_enabled "
-                            + "FROM customer "
-                            // Limits to 1 to clarify that selecting from the customer resource
-                            // will always return only one row, which will be for the customer
-                            // ID specified in the request.
-                            + "LIMIT 1";
+                            + "FROM customer ";
+            Long customerId = googleAdsClient.getLoginCustomerId();
             // Executes the query and gets the Customer object from the single row of the response.
             GoogleAdsServiceClient.SearchPagedResponse response =
                     googleAdsServiceClient.search(Long.toString(customerId), query);
             GoogleAdsRow googleAdsRow = response.iterateAll().iterator().next();
             Customer customer = googleAdsRow.getCustomer();
-            // Prints account information.
+//             Prints account information.
             System.out.printf(
                     "Customer with ID %d, descriptive name '%s', currency code '%s', timezone '%s', "
                             + "tracking URL template '%s' and auto tagging enabled '%s' was retrieved.%n",
@@ -105,9 +95,71 @@ public class GetCustomerInfo {
                     customer.getTrackingUrlTemplate(),
                     customer.getAutoTaggingEnabled());
 
+//            saveDataToFile("jsonName",customer.toString());
 
-
+//            try {
+//                Connection connection = MySqlConnt.init();
+//                // 执行查询
+//                Statement stmt = null;
+//                System.out.println(" create Statement...");
+//                stmt = connection.createStatement();
+//                String sql;
+//                sql = "SELECT * from 01_cmsdata_dev1.admin_role ";
+//                ResultSet rs = stmt.executeQuery(sql);
+//
+//                // 展开结果集数据库
+//                while(rs.next()){
+//                    // 通过字段检索
+//                    int id  = rs.getInt("id");
+//                    String name = rs.getString("name");
+//
+//                    // 输出数据
+//                    System.out.print("ID: " + id);
+//                    System.out.print(", 站点名称: " + name);
+//                    System.out.print("\n");
+//                }
+//                // 完成后关闭
+//                rs.close();
+//                stmt.close();
+//                connection.close();
+//            } catch (ClassNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (SQLException e) {
+//                // 处理 JDBC 错误
+//                e.printStackTrace();
+//            }
+//
 
         }
+    }
+
+
+    private void saveDataToFile(String fileName,String data) {
+        BufferedWriter writer = null;
+        File file = new File("c://"+ fileName + ".text");
+                //如果文件不存在，则新建一个
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //写入
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,false), "UTF-8"));
+            writer.write(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(writer != null){
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("文件写入成功！");
     }
 }
